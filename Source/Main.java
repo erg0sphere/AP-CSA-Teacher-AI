7943bf2fdbff8759a6c353823cc6d8fa72523f34
Danie1 <92999807+erg0sphere@users.noreply.github.com>
2023-03-03 21:05:51 -0800

    Add files via upload

---
+++ Source/Main.java
@@ -0,0 +1,57 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+class Main
+{
+  public static void main(String[] args)
+  {
+    ArrayList<String> keyWords = new ArrayList<String>(); //creates the array list
+
+    keyWords.add("what");
+    keyWords.add("who");
+    keyWords.add("why");
+    keyWords.add("where");
+    keyWords.add("how");
+    keyWords.add("who");
+    keyWords.add("which");
+    keyWords.add("so");
+    keyWords.add("yeah");
+    keyWords.add("at");
+    keyWords.add("explain");
+    keyWords.add("have");
+    keyWords.add("come");
+    keyWords.add("if");
+    keyWords.add("get");
+    keyWords.add("to");
+    keyWords.add("is");
+    keyWords.add("tell");
+    keyWords.add("give");
+    keyWords.add("provide");
+    keyWords.add("when");
+    keyWords.add("momentum");
+    keyWords.add("optics");
+    keyWords.add("progress");
+    keyWords.add("checks");
+    keyWords.add("alright");
+    keyWords.add("ap");
+    keyWords.add("classroom");
+    keyWords.add("quiz");
+    keyWords.add("test");
+    keyWords.add("frq");
+    keyWords.add("mcq");
+    keyWords.add("sam");
+    keyWords.add("we");
+    keyWords.add("have");
+    keyWords.add("circuits");
+    keyWords.add("lab");
+    keyWords.add("warm");
+    keyWords.add("up");
+    keyWords.add("force");
+    keyWords.add("did");
+    keyWords.add("can");
+    
+    new GUI(); //creates the gui
+  
+    
+  }
+}
